d47a281713ef96abacf7f66041bf592721bdd68c
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-18 15:14:46 -0300

    Section 3, class 27

--- exercicios_react/ex/family.jsx
+++ exercicios_react/ex/family.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { childrenWithProps } from '../utils/reactUtils';
 
 export default props => (
     <div>
         <h1>Family</h1>
-        { React.cloneElement(props.children, {...props}) }
+        { childrenWithProps(props.children, props) }
     </div>
 );
