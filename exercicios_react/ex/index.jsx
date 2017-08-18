df1c0a36bbe3db69e982c9efc9679333f3990327
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-18 14:44:07 -0300

    Section 3, class 25

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SilvaFamily from './silvaFamily';
+import Family from './family'
+import Member from './member'
 
 ReactDOM.render(
-    <SilvaFamily />
+    <Family>
+        <Member name='Guilherme' lastName='Silva' />
+    </Family>
 , document.getElementById('app'));
--- exercicios_react/ex/member.jsx
+++ exercicios_react/ex/member.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default props => (
     <div>
+        <h1>Family</h1>
         {props.name} <strong>{props.lastName}</strong>
     </div>
 );
