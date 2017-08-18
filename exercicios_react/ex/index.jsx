5332f4c2fe30ca80c308f757bed2b15c0e251338
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-18 16:04:09 -0300

    Section 3, class 30

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ClassComponent from './classComponent';
+import Field from './field';
 
 ReactDOM.render(
-    <ClassComponent label='Contador' initialValue={10} />
+    <Field label='Contador' initialValue={10} />
 , document.getElementById('app'));
