43731ae79fb9f3cbc1c2352298c59e23472a1bc8
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-18 02:09:56 -0300

    Section 3, class 20

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Component from './component';
 
-ReactDOM.render(<h1>Olá React!</h1>, document.getElementById('app'));
+ReactDOM.render(<Component />, document.getElementById('app'));
