87ce65780e3c6b7d94d92be5197807d0bdcce41f
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-20 20:56:27 -0300

    Section 6, class 63

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import Field from './field';
 
+const reducers = combineReducers({
+    field: () => ({value: 'Opa'})
+});
+
 ReactDOM.render(
-    <Field label='Contador' initialValue={10} />
-, document.getElementById('app'));
+    <Provider store={createStore(reducers)}>
+        <Field label='Contador' initialValue={10} />
+    </Provider>
+
+,document.getElementById('app'));
