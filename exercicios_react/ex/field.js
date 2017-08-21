854c4dca62ac1880cf4d1240183330d5f726015f
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-20 21:57:00 -0300

    Section 6, class 65

--- exercicios_react/ex/field.js
+++ exercicios_react/ex/field.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { changeValue } from './fieldAction';
 
 class Field extends Component {
     render() {
@@ -7,7 +9,7 @@ class Field extends Component {
             <div>
                 <label>{this.props.value}</label>
                 <br />
-                <input onChange={this.handleChange} value={this.props.value} />
+                <input onChange={this.props.changeValue} value={this.props.value} />
             </div>
         );
     }
@@ -19,4 +21,8 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(Field);
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({changeValue}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Field);
---
+++ exercicios_react/ex/fieldAction.jsx
@@ -0,0 +1,8 @@
+export function changeValue(e) {
+    console.log('changeValue');
+
+    return {
+        type: 'VALUE_CHANGED',
+        payload: e.target.value
+    };
+}
