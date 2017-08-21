37d884048b2ca87908d993576604cda591e399e4
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-20 22:18:27 -0300

    Section 6, class 66

--- exercicios_react/ex/field.js
+++ exercicios_react/ex/field.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { changeValue } from './fieldAction';
+import { changeValue } from './fieldActions';
 
 class Field extends Component {
     render() {
--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import fieldReducer from './fieldReducer';
 
 import Field from './field';
 
 const reducers = combineReducers({
-    field: () => ({value: 'Opa'})
+    field: fieldReducer
 });
 
 ReactDOM.render(
